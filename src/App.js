82cc875808fd753214c1ef0902951aa29f117820
VishnuXT <vishnu.nrd1234@gmail.com>
2024-07-25 23:04:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './Components/index';
+import Search from './Components/search';
+import Viewall from './Components/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<First/>}/>
+          <Route path="/search" element={<Search/>}/>
+          <Route path="/view" element={<Viewall/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
